84f159ecad455732b8a8dac7503259bec4ba9fd1
mohammad shuvo <mohammadmasud34@gmail.com>
2023-03-14 15:56:47 +0600

    location get added

--- app/Http/Controllers/Api/V1/LocationController.php
+++ app/Http/Controllers/Api/V1/LocationController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Api\V1\Helper\ApiFilter;
 use App\Http\Controllers\Api\V1\Helper\ApiResponse;
 use App\Models\Location\District;
+use App\Models\Location\StreetAddress;
+use App\Models\Location\Thana;
+use App\Models\Location\Union;
+use App\Models\Location\Zipcode;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Response as HttpResponse;
 use PhpParser\Node\Stmt\TryCatch;
@@ -34,11 +38,72 @@ class LocationController extends Controller
     public function districts(Request $request)
     {
         try {
-            $query = District::select('id', 'district_name', 'district_slug');
+            $query = District::select('id', 'state_id', 'district_name', 'district_slug');
             $query = $this->filterBy($request, $query);
             $districts = $this->query->get();
             if (count($districts) > 0) {
                 return $this->success($districts);
+            } else {
+                return $this->error("Data Not Found", 404);
+            }
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage(), 422);
+        }
+    }
+    public function thanas(Request $request)
+    {
+        try {
+            $query = Thana::select('id', 'district_id', 'thana_name', 'thana_slug');
+            $query = $this->filterBy($request, $query);
+            $thanas = $this->query->get();
+            if (count($thanas) > 0) {
+                return $this->success($thanas);
+            } else {
+                return $this->error("Data Not Found", 404);
+            }
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage(), 422);
+        }
+    }
+
+    public function unions(Request $request)
+    {
+        try {
+            $query = Union::select('id', 'thana_id', 'union_name', 'union_slug');
+            $query = $this->filterBy($request, $query);
+            $unions = $this->query->get();
+            if (count($unions) > 0) {
+                return $this->success($unions);
+            } else {
+                return $this->error("Data Not Found", 404);
+            }
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage(), 422);
+        }
+    }
+    public function zipcodes(Request $request)
+    {
+        try {
+            $query = Zipcode::select('id', 'union_id', 'zip_code');
+            $query = $this->filterBy($request, $query);
+            $zipcodes = $this->query->get();
+            if (count($zipcodes) > 0) {
+                return $this->success($zipcodes);
+            } else {
+                return $this->error("Data Not Found", 404);
+            }
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage(), 422);
+        }
+    }
+    public function streetAdress(Request $request)
+    {
+        try {
+            $query = StreetAddress::select('id', 'union_id', 'street_address_value');
+            $query = $this->filterBy($request, $query);
+            $streetAdress = $this->query->get();
+            if (count($streetAdress) > 0) {
+                return $this->success($streetAdress);
             } else {
                 return $this->error("Data Not Found", 404);
             }
--- routes/customers.php
+++ routes/customers.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\CustomerController;
+use Illuminate\Support\Facades\Route;
 
 Route::POST('', [CustomerController::class, 'create'])->name('customer.create');
 Route::GET('', [CustomerController::class, 'index'])->name('customers.index');
